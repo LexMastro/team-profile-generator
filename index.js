5635b4d0dcc7993fab5ec4e279b382a7ec6993b6
Alexis Mastrodomenico <86946841+LexMastro@users.noreply.github.com>
2021-10-27 14:28:49 +1100

    adding to index file

--- index.js
+++ index.js
@@ -13,52 +13,105 @@ const util = require('util');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const teamManager = () => {
+const promptQuestions = () => {
     return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Who is the Manager of this team?',
-        validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter a manager to continue'}}
-      },
-      {
-        type: 'input',
-        name: 'id',
-        message: 'Please enter the managers ID.',
-        validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter a managers ID to continue'}}
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter the managers email.',
-        validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter a managers email to continue'}}
-      },
-      {
-        type: 'input',
-        name: 'office',
-        message: 'Please enter the managers office number',
-        validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter a managers office number to continue'}}
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username',
-      },
-      {
-        type: 'input',
-        name: 'linkedin',
-        message: 'Enter your LinkedIn URL.',
-      },
-    ]);
-  };
+        {
+            type: "list",
+            name: "role",
+            message: "What is your job role?",
+            choices: ["Manager", "Engineer", "Intern"],
+            validate: (value) =>{ if(value !== "") {return true} else {return 'Please choose a role to continue'}}
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What is your ID?",
+            validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter ID to continue'}}
+        },
+        {
+            type: "list",
+            name: "email",
+            message: "What is your email?",
+            validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter email to continue'}}
+        },
 
+        ]).then(data => {
+            switch (data.role) {
+                case "Manager":
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your office number?",
+                        name: "office",
+                        validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter office number to continue'}}
+                    },
+                ]).then[addEmployee => {
+                    ` <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                      <meta charset="UTF-8">
+                      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+                      <link rel="stylesheet" href="fontawesome.min.css">
+                      <link rel="stylesheet" href="style.css">
+                      <title>Team Profile Generator</title>
+                    </head>
+                    <body>
+                        <header>
+                            <h1>My Team</h1>
+                        </header>
+                        <div class="container">
+                        <div class="card">
+                            <div class="card-title-bg">
+                            <h1 class="card-title">${answers.name}</h1>
+                            </div>
+                            <div class="card-info">
+                              <i class="fas fa-tasks"></i>
+                              <p class="id">ID: ${answers.id}</p>
+                              <p class="email">Email: <a href="mailto:${answers.email}">${answers.email}</a></p>
+                              <p class="office">Office Number: ${answers.office}</p>
+                              </div>
+                          </div>
+                        </div>
+                    </body>
+                    </html>`;
+                    addEmployee(Manager.answers);
+                }]
+            }
+            switch (data.role) {
+                case "Engineer":
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your github username?",
+                        name: "github",
+                        validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter github username to continue'}}
+                    },
+                ]);
+                addEmployee(Engineer.answers);
+            }
+            switch (data.role) {
+                case "Intern":
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What school did you study at?",
+                        name: "school",
+                        validate: (value) =>{ if(value !== "") {return true} else {return 'Please enter school to continue'}}
+                    },
+                ]);
+                addEmployee(Intern.answers);
+            }
+        })
+    }
   const generateHTML = (answers) =>
-  `<!DOCTYPE html>
+  ` <!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome.min.css">
     <link rel="stylesheet" href="style.css">
     <title>Team Profile Generator</title>
   </head>
@@ -66,35 +119,50 @@ const teamManager = () => {
       <header>
           <h1>My Team</h1>
       </header>
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
       <div class="container">
-        <div class="row">
-          <div class="col">
-            <h1 class="display-4">${answers.name}</h1>
-            <p class="lead">ID: ${answers.id}.</p>
-            <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-            <ul class="list-group">
-              <li class="list-group-item">My GitHub username is ${answers.github}</li>
-              <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-            </ul>
+      <div class="card">
+          <div class="card-title-bg">
+          <h1 class="card-title">${answers.name}</h1>
           </div>
+          <div class="card-info">
+            <i class="fas fa-tasks"></i>
+            <p class="id">ID: ${answers.id}</p>
+            <p class="email">Email: <a href="mailto:${answers.email}">${answers.email}</a></p>
+            <p class="office">Office Number: ${answers.office}</p>
+            </div>
         </div>
       </div>
-    </div>
-  </div>
   </body>
   </html>`;
 
 
-  const init = () => {
-    teamManager()
-    teamEngineer()
-    teamIntern()
-      .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-      .then(() => console.log('Successfully wrote to index.html'))
-      .catch((err) => console.error(err));
-  };
+//   const init = () => {
+//     promptQuestions()
+//       .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+//       .then(() => console.log('Successfully made your team!'))
+//       .catch((err) => console.error(err));
+//   };
+
+  function init() {
+    promptQuestions()
+    .then((answers) => {
+
+    const htmlFile = generateMarkdown(answers);
+    console.log(readme)
+    const fileName = "index.html"
+    const outputPath = path.join(__dirname, 'output')
+
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath, 0744);
+    }
+
+    const filePath = path.join(outputPath, fileName)
+
+    fs.writeFileSync(filePath, htmlFile, (err) =>
+    err ? console.log(err) : console.log(`Successfully created ${fileName}!`)  
+        )
+    });
+ };
   
   init();
   
